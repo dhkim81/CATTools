bf420482bfe634aa44642a0ca74293a17dfb2d1d
DongHyun Kim <dong.hyun.kim@cern.ch>
2018-04-24 23:24:44 +0900

    Update TopTriggerSF.cc
    Scale factor update from AN2016/392_v3

--- CatAnalyzer/src/TopTriggerSF.cc
+++ CatAnalyzer/src/TopTriggerSF.cc
@@ -3,7 +3,8 @@
 #include "CATTools/CommonTools/interface/TTbarModeDefs.h"
 #include <cmath>
 
-// Scale factors from AN2015/309
+// Scale factors from AN2015/309 (OLD)
+// Scale factors from AN2016/392_v3 (Update)
 double computeTrigSF(const cat::Lepton& lep1, const cat::Lepton& lep2, int direction)
 {
   const int sumId = std::abs(lep1.pdgId()) + std::abs(lep2.pdgId());
@@ -18,32 +19,32 @@ double computeTrigSF(const cat::Lepton& lep1, const cat::Lepton& lep2, int direc
 
   if ( channel == cat::CH_ELEL ) {
     if ( aeta1 < 1.2 ) {
-      if      ( aeta2 < 1.2 ) return 0.953 + direction*0.023;
-      else if ( aeta2 < 2.4 ) return 0.957 + 0.5*direction*0.027;
+      if      ( aeta2 < 1.2 ) return 0.990 + direction*0.002;
+      else if ( aeta2 < 2.4 ) return 0.990 + 0.5*direction*0.003;
     }
     else if ( aeta1 < 2.4 ) {
-      if      ( aeta2 < 1.2 ) return 0.967 + 0.5*direction*0.029;
-      else if ( aeta2 < 2.4 ) return 0.989 - direction*0.031;
+      if      ( aeta2 < 1.2 ) return 0.995 + 0.5*direction*0.003;
+      else if ( aeta2 < 2.4 ) return 0.979 - direction*0.007;
     }
   }
   else if ( channel == cat::CH_MUMU ) {
     if ( aeta1 < 1.2 ) {
-      if      ( aeta2 < 1.2 ) return 0.926 + direction*0.022;
-      else if ( aeta2 < 2.4 ) return 0.943 + 0.5*direction*0.026;
+      if      ( aeta2 < 1.2 ) return 0.991 + direction*0.001;
+      else if ( aeta2 < 2.4 ) return 0.990 + 0.5*direction*0.003;
     }
     else if ( aeta1 < 2.4 ) {
-      if      ( aeta2 < 1.2 ) return 0.958 + 0.5*direction*0.027;
-      else if ( aeta2 < 2.4 ) return 0.926 - direction*0.027;
+      if      ( aeta2 < 1.2 ) return 0.995 + 0.5*direction*0.003;
+      else if ( aeta2 < 2.4 ) return 0.987 - direction*0.003;
     }
   }
   else if ( channel == cat::CH_MUEL ) {
     if ( aeta1 < 1.2 ) {
-      if      ( aeta2 < 1.2 ) return 0.966 + direction*0.022;
-      else if ( aeta2 < 2.4 ) return 0.973 + 0.5*direction*0.025;
+      if      ( aeta2 < 1.2 ) return 0.996 + direction*0.001;
+      else if ( aeta2 < 2.4 ) return 0.999 + 0.5*direction*0.003;
     }
     else if ( aeta1 < 2.4 ) {
-      if      ( aeta2 < 1.2 ) return 0.981 + 0.5*direction*0.025;
-      else if ( aeta2 < 2.4 ) return 0.984 - direction*0.030;
+      if      ( aeta2 < 1.2 ) return 0.995 + 0.5*direction*0.003;
+      else if ( aeta2 < 2.4 ) return 0.990 - direction*0.006;
     }
   }
 
@@ -51,14 +52,15 @@ double computeTrigSF(const cat::Lepton& lep1, const cat::Lepton& lep2, int direc
 };
 
 // Scale factors are from AN16-025 (v4) http://cms.cern.ch/iCMS/jsp/openfile.jsp?tp=draft&files=AN2016_025_v4.pdf
+// Scale factors are from AN2016/392_v3 (Update) page No.5
 double computeTrigSFInclusive(const cat::Lepton& lep1, const cat::Lepton& lep2, int direction)
 {
   const int sumId = std::abs(lep1.pdgId()) + std::abs(lep2.pdgId());
   const int channel = sumId == 11+11 ? cat::CH_ELEL : sumId == 13+13 ? cat::CH_MUMU : cat::CH_MUEL;
 
-  if      ( channel == cat::CH_ELEL ) return 0.953 + direction*0.009;
-  else if ( channel == cat::CH_MUMU ) return 0.948 + direction*0.002;
-  else if ( channel == cat::CH_MUEL ) return 0.975 + direction*0.004;
+  if      ( channel == cat::CH_ELEL ) return 0.996 + direction*0.001;
+  else if ( channel == cat::CH_MUMU ) return 0.990 + direction*0.001;
+  else if ( channel == cat::CH_MUEL ) return 0.996 + direction*0.001;
 
   return 1;
 }
